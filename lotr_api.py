7e4f4343efdf7ef4cd5a150b7cdf523a863e4fa6
Jonnyteronni <antero.soeiro@gmail.com>
2020-07-16 07:48:04 +0100

    calculations for married characters

--- lotr_api.py
+++ lotr_api.py
@@ -27,6 +27,7 @@ df[(df['gender']!='Female') &
 df[df['gender']==''] = 'No info'
 
 # Count per gender
+print('Count total characters per gender')
 print(df[['name','gender']].groupby(by='gender').agg('count'))
 print('\n')
 
@@ -41,26 +42,56 @@ pov_list = ['Frodo Baggins','Samwise Gamgee','Bilbo Baggins','Peregrin Took',
 pov_df = df[df['name'].isin(pov_list)]
 
 # Count pov characters per gender
+print('Count pov characters per gender')
 print(pov_df[['name','gender']].groupby(by='gender').agg('count'))
 print('\n')
 
 # Death Count
 life_words = ['FO','TA','alive','departed']
 
-
-
-# Count deaths on pov population
-pov_death_df = pov_df[['name','death']].copy()
-deaths = 0
+# Count deaths on total population
+total_alive_df = df[['name','death']].copy()
+alive = 0
 for number in range(len(life_words)):
-    deaths += pov_death_df[pov_death_df['death'].str.contains(life_words[number])]['death'].count()
+    alive += total_alive_df[total_alive_df['death'].str.contains(life_words[number]) |
+    total_alive_df['death'].isna()]['death'].count()
+
+df_total = df['name'].count()
+print('{} characters alive on total population from a total of {}.\n\n'.format(alive,df_total))
+
+
+# Count alives on pov population
+pov_alive_df = pov_df[['name','death']].copy()
+alive = 0
+for number in range(len(life_words)):
+    alive += pov_alive_df[pov_alive_df['death'].str.contains(life_words[number])]['death'].count()
 
 pov_total = pov_df['name'].count()
-print('{} deaths on pov population from a total of {}.'.format(deaths,pov_total))
+print('{} characters alive on pov population from a total of {}.\n\n'.format(alive,pov_total))
+
+# Spouse count on total population
+#print(df[df['spouse']=='']['_id'].index)
+#print(df[df['spouse'].isna()]['_id'].index)
+
+#Check for duplicates
+# for element in df[df['spouse'].isna()]['_id'].index:
+#     if element in df[df['spouse']=='']['_id'].index:
+#         print(True)
+total_married = df[df['spouse']=='']['_id'].count() + df[df['spouse'].isna()]['_id'].count()
+print('{} characters married on total population from a total of {}.\n\n'.format(total_married,df_total))
 
 
-# Count deaths on pov population
-# print(pov_df[['name','death']].count())
-# print('\n')
-#
-# print(pov_df[['name','death']])
+# Spouse count on pov population
+pov_married = pov_df[pov_df['spouse']=='']['_id'].count() + pov_df[pov_df['spouse'].isna()]['_id'].count()
+print('{} characters married on total population from a total of {}.\n\n'.format(pov_married,pov_total))
+
+
+# print('-----')
+# print(pov_df[pov_df['spouse'].isna()]['_id'].count())
+# print(('-----'))
+# print(pov_df[pov_df['spouse']=='']['_id'].count())
+# #print(pov_df['spouse'])
+# print('-----')
+# print(df[df['spouse'].isna()]['spouse'])
+# print('-----')
+# print(pov_df[['name','spouse']])
